2e62f3c86ab9e5506e2782e9c7b63cf9ae888f62
madam31-rrc <madam31@academic.rrc.ca>
2025-11-02 11:24:28 -0600

    feat: update loanRoutes files

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import * as loanController from '../controllers/loanControllers';
- 
-const router = Router();
- 
-router.post('/', loanController.createLoan)
-router.put('/:id/review', loanController.reviewLoan);
+import { loanController } from '../controllers/loanControllers';
+
+const router = Router();;
+
+router.post('/', loanController.createLoan);
+
 router.get('/', loanController.getLoans);
+
+router.put('/:id/review', loanController.reviewLoan);
+
 router.put('/:id/approve', loanController.approveLoan);
- 
+
 export default router;
