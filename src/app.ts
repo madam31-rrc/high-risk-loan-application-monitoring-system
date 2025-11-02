1001108ad5092711c2b7459bc049ea8531fe9359
madam31-rrc <madam31@academic.rrc.ca>
2025-11-02 11:23:16 -0600

    feat: update app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,26 @@
 import express from "express";
 import morgan from "morgan";
 import loanRoutes from "./api/v1/routes/loanRoutes";
+import { Server } from "http";
 
+const PORT = parseInt(process.env.PORT as string) || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
 
-// health
 app.get("/health", (_req, res) => res.status(200).send("Server is healthy"));
 
-// API
 app.use("/api/v1/loans", loanRoutes);
 
-// error handler
 app.use((err: any, _req: express.Request, res: express.Response, _next: any) => {
   console.error(err);
   const status = err?.status || 500;
   res.status(status).json({ error: err?.message ?? "Internal server error" });
 });
 
-export default app;
+const server: Server = app.listen(PORT, '0.0.0.0', 0, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export  {app, server};
