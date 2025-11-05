209b26d8369699fb16e6d0c07b652c582b0223e1
madam31-rrc <madam31@academic.rrc.ca>
2025-11-04 23:37:17 -0600

    Feat: loanRoute update

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { loanController } from '../controllers/loanControllers';
+import isAuthorized from '../middleware/authorize';
+import authenticate from '../middleware/authenticate';
 
 const router = Router();
 
-router.post('/', loanController.createLoan);
+router.post('/', authenticate,
+    isAuthorized({ hasRole: ["user"] }), loanController.createLoan);
 
-router.get('/', loanController.getLoans);
+router.get('/', authenticate,
+    isAuthorized({ hasRole: ["officer", "manager"] }), loanController.getLoans);
 
-router.put('/:id/review', loanController.reviewLoan);
+router.put('/:id/review',authenticate,
+    isAuthorized({ hasRole: ["officer"] }), loanController.reviewLoan);
 
-router.put('/:id/approve', loanController.approveLoan);
+router.put('/:id/approve',authenticate,
+    isAuthorized({ hasRole: [ "manager"] }), loanController.approveLoan);
 
 export default router;
